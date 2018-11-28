5be8ab3b34da5e1caec0eaac187e7a8f285b1103
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-28 15:15:49 +0800

    Added statement

---
+++ assessment_1.py
@@ -0,0 +1,21 @@
+"""
+Replace the contents of this module docstring with your own details.
+"""
+
+MENU = ("L - List songs\nA - Add new song\nC - Complete a song\nQ - Quit\n\nPlease select your choices:\n>>> ")
+
+def main():
+    print("Songs To Learn 1.0 - by <Kokfong Hav>")
+    choice = input(MENU).upper()
+    while choice != "Q":
+        if choice == "L":
+            print("This is your songs list:")
+        elif choice == "A":
+            print()
+        elif choice == "C":
+            print()
+        else:
+            print("Please enter the valid letter.")
+            choice
+    exit()
+main()
